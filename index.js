7d873ba43e67ea995eb9b32277484291eebd84fe
Neirel Zapatos <neirelzapatos@gmail.com>
2024-08-11 21:25:06 -0700

    changed google oauth callback

--- index.js
+++ index.js
@@ -372,7 +372,7 @@ passport.use("google",
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/auth/google/success",
+            callbackURL: "https://book-notes-0jmk.onrender.com/auth/google/success",
             userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
         },
         async (accessToken, refreshToken, profile, cb) => {
